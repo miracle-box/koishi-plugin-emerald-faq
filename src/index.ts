bd44b0bbeed7fb5c6caccfedc7bc78ba3999710b
Nixieboluo <me@sagirii.me>
2024-05-17 20:14:48 +0800

    feat: this is emerald-faq pluigin

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,60 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema } from "koishi";
 
-export const name = 'emerald-faq'
+export const name = "emerald-faq";
 
-export interface Config {}
+export interface Config {
+  defaultLocale: string;
+  items: Record<
+    string,
+    {
+      desc: string;
+      content: string;
+    }
+  >;
+}
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config: Schema<Config> = Schema.object({
+  defaultLocale: Schema.string().default("zh-CN"),
+  items: Schema.dict(
+    Schema.object({
+      desc: Schema.string(),
+      content: Schema.string(),
+    })
+  ).role("table"),
+}).i18n({
+  "zh-CN": require("./translation/zh-CN.yaml")._config,
+});
 
 export function apply(ctx: Context) {
-  // write your plugin here
+  // Regular i18n dict
+  ctx.i18n.define("zh-CN", require("./translation/zh-CN.yaml"));
+
+  // I18n entry for faq items
+  const faqDict = { faq: { items: (ctx.config as Config).items } };
+  ctx.i18n.define((ctx.config as Config).defaultLocale, faqDict);
+
+  ctx.command("faq [item]").action(({ session }, item) => {
+    // Flatten faq items for use
+    const items = Object.entries((ctx.config as Config).items).map(
+      ([key, { desc, content }]) => ({
+        key,
+        desc,
+        content,
+      })
+    );
+
+    // faq listing
+    if (!item || item === "list") {
+      if (items.length <= 0) return session.text(".noitems");
+
+      return session.text(".list", { items });
+    }
+
+    const faqItemExists = !!items.find(({ key }) => item === key);
+    if (!faqItemExists) return session.text(".notfound", [item]);
+
+    return session.text(getItemContentI18nKey(item));
+  });
 }
+
+const getItemContentI18nKey = (key: string) => `faq.items.${key}.content`;
